5da123ab238fe1535eaabae0a1da292207064123
Barry A Bragg Jr <hokieg3n1us@gmail.com>
2021-09-14 17:45:16 -0400

    Update Example

--- motionshader_example.py
+++ motionshader_example.py
@@ -12,7 +12,7 @@ import motionshader
 if __name__ == '__main__':
     # Motionshader expects a time indexed, sorted, Dask DataFrame with columns containing EPSG:4326 coordinates.
     # Motionshader is opinionated about using Dask DataFrames, for scaling this process to big data.
-    df = dask.dataframe.read_csv('NYC.csv', usecols=['timestamp', 'longitude', 'latitude'])
+    df = dask.dataframe.read_csv('Syria-Simulated.csv', usecols=['timestamp', 'longitude', 'latitude'])
     df['timestamp'] = dask.dataframe.to_datetime(df['timestamp'])
     df = df.set_index('timestamp', npartitions=mp.cpu_count(), compute=True).persist()
 
@@ -28,12 +28,16 @@ if __name__ == '__main__':
     # Define the MotionVideo, providing the dataset to be rendered and the basemap service.
     motion_video = motionshader.MotionVideo(dataset, basemap)
 
+    start_datetime, end_datetime = df.index.min().compute(), df.index.max().compute()
+    min_longitude, max_longitude = df.longitude.min().compute(), df.longitude.max().compute()
+    min_latitude, max_latitude = df.latitude.min().compute(), df.latitude.max().compute()
+
     # GeospatialViewport allows defining where the 'camera' looks on the globe, and the resolution of the output.
     # TemporalPlayback defines the temporal bounds, temporal size of a frame, and the frames per second for the output.
     # In this example, a single frame contains 30 minutes of data, and steps forward 15 minutes between frames.
-    viewport = motionshader.GeospatialViewport(-74.25909, -73.700181, 40.477399, 40.916178, 1920, 1080)
-    playback = motionshader.TemporalPlayback(datetime(2020, 4, 1), datetime(2020, 4, 2), timedelta(minutes=30),
-                                             timedelta(minutes=15), 5)
+    viewport = motionshader.GeospatialViewport(min_longitude, max_longitude, min_latitude, max_latitude, 1920, 1080)
+    playback = motionshader.TemporalPlayback(start_datetime, end_datetime, timedelta(seconds=15),
+                                             timedelta(seconds=5), 5)
 
     # If a FrameAnnotation is provided to the rendering function, the time range and center coordinate will be
     # added onto each frame. The FrameAnnotation allows customizing the position of the label in pixel coordinates,
@@ -49,5 +53,5 @@ if __name__ == '__main__':
                                             '#000000')
 
     # MotionVideo can be output as either a GIF or an MP4.
-    motion_video.to_gif(viewport, playback, 'NYC', annotation, watermark, 1, colorcet.fire)
-    motion_video.to_video(viewport, playback, 'NYC', annotation, watermark, 1, colorcet.fire)
+    motion_video.to_gif(viewport, playback, 'Syria', annotation, watermark, 1, colorcet.fire)
+    motion_video.to_video(viewport, playback, 'Syria', annotation, watermark, 1, colorcet.fire)
